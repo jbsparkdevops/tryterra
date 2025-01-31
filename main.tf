84f2e0b36d9fcba9bd81f6a6cd700e12757dab60
jbsparkdevops <bharghav588@gmail.com>
2025-01-31 14:07:30 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,25 +9,3 @@ resource "google_project_service" "secretmanager" {
   service           = "secretmanager.googleapis.com"
   disable_on_destroy = false
 }
-
-
-provider "google" {
-  project = var.project_id
-  region  = "us-central1"
-}
-
-resource "google_compute_network" "vpc_network" {
-  name                    = "custom-vpc"
-  auto_create_subnetworks = false
-}
-
-resource "google_compute_subnetwork" "proxy_only_subnet" {
-  name          = "proxy-only-subnet"
-  network       = google_compute_network.vpc_network.id
-  region        = "us-central1"
-  purpose       = "REGIONAL_MANAGED_PROXY"
-  role          = "ACTIVE"
-  ip_cidr_range = "10.129.0.0/23"
-
-  depends_on = [google_compute_network.vpc_network]
-}
