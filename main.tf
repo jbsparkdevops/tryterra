a3b051596d5b48cba3ef8e5a59dde97566add89d
jbsparkdevops <bharghav588@gmail.com>
2025-02-01 10:09:23 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,15 +10,4 @@ resource "google_project_service" "secretmanager" {
   disable_on_destroy = true
 }
 
-resource "google_storage_bucket" "terraform-state-bucket" {
-  project       = var.project_id
-  name          = "bharghav-bucket-1234"
-  location      = "us-central1"
-  force_destroy = true
 
-  uniform_bucket_level_access = true
-
-  cors {
-    max_age_seconds = 3600
-  }
-}
