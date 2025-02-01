c60bedf021c993f0986f89ae2ba43624bdb1aa64
jbsparkdevops <bharghav588@gmail.com>
2025-02-01 10:08:10 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ resource "google_project_service" "iam" {
 resource "google_project_service" "secretmanager" {
   project           = var.project_id
   service           = "secretmanager.googleapis.com"
-  disable_on_destroy = false
+  disable_on_destroy = true
 }
 
 resource "google_storage_bucket" "terraform-state-bucket" {
@@ -22,23 +22,3 @@ resource "google_storage_bucket" "terraform-state-bucket" {
     max_age_seconds = 3600
   }
 }
-provider "google" {
-  project = var.project_id
-  region  = "us-central1"
-}
-
-resource "google_compute_network" "vpc_network" {
-  name                    = "custom-vpc"
-  auto_create_subnetworks = false
-}
-resource "google_compute_subnetwork" "proxy_only_subnet" {
-  name          = "proxy-only-subnet"
-  network       = google_compute_network.vpc_network.id
-  region        = "us-central1"
-  purpose       = "REGIONAL_MANAGED_PROXY"
-  role          = "ACTIVE"
-  ip_cidr_range = "10.129.0.0/23"
-
-  depends_on = [google_compute_network.vpc_network]
-}
-
